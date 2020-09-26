345e09e547294722b160086fe4590156b965a389
Andrew <mad.scientise@gmail.com>
2020-09-26 11:35:29 +0300

    delete needless elements

--- js/web.js
+++ js/web.js
@@ -25,26 +25,15 @@ ws.on('request', req => {
   const connection = req.accept('', req.origin);
   clients.push(connection);
   console.log('Connected ' + connection.remoteAddress);
-//   connection.on('close', onWsClose );
   connection.on('message', onWsMessage);
-//   connection.on('error', onWsError);
-
-//   function onWsError(e) {
-//     console.error(e.message);
-//   }
 
   function onWsMessage(message) {
     const dataName = message.type + 'Data';
     const data = message[dataName];
-    //console.log('Received: ' + data);
     clients.forEach(client => {
       if (connection !== client) {
         client.send(data);        
       }     
     });
   }
-//   function onWsClose(reasonCode, description) {
-//     console.log('Disconnected ' + connection.remoteAddress);
-//     console.dir({ reasonCode, description });
-//  }
 });
